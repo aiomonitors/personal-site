cf18fbe9dbbf85a9b4c3a9e5f0c75ca518ab6f15
Shihab Chowdhury <sachow168@gmail.com>
2020-04-01 17:47:20 -0400

    Production build

---
+++ server/build/precache-manifest.eef5e17248fdd5e445be5c345f10d2e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a030dd957f8dce3f7cdd9e2ee8493f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1b7401081f37c1506fd",
+    "url": "/static/css/main.5cd0314c.chunk.css"
+  },
+  {
+    "revision": "464ac7b7d5d70590e909",
+    "url": "/static/js/2.988d0be5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.988d0be5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1b7401081f37c1506fd",
+    "url": "/static/js/main.5c7ba2b5.chunk.js"
+  },
+  {
+    "revision": "7d4a7932cfdcd47c370c",
+    "url": "/static/js/runtime-main.0128b0c7.js"
+  }
+]);
